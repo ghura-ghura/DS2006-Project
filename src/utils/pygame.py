c8b23df8f615ec6499b45ba0b043e7df512fc66c
CrawlingHam <crawlingham@gmail.com>
2025-10-21 10:10:24 +0200

    Added additional custom types and utility functions

--- src/utils/pygame.py
+++ src/utils/pygame.py
@@ -1,7 +1,10 @@
-from src.types.main import ColorFunction, SelectorFunction
+from src.types.main import ColorFunction, SelectorFunction, PyGameEventFunction
 from src.menu.config import COLORS
+from sys import exit
 from pygame import (
-    Surface, font, Rect
+    Surface, font, Rect, quit as pygame_quit,
+    QUIT, KEYDOWN, MOUSEBUTTONDOWN,
+    event as pygame_event,
 )
 
 def draw_options(surface: Surface, options: list[str], title_position: tuple[int, int], font: font.Font, margin_top: int, gap: int, alpha: int | None = None, color_fn: ColorFunction | None = None, selector_fn: SelectorFunction | None = None) -> None:
@@ -67,3 +70,77 @@ def draw_title(surface: Surface, title: str, font: font.Font, color: tuple[int, 
 
     title_rect: Rect = title_surface.get_rect(center=position)
     surface.blit(title_surface, title_rect)
+
+def handle_event(on_key_down: PyGameEventFunction, on_mouse_down: PyGameEventFunction) -> None:
+    """
+    Summary:
+    
+    Handles the events and calls the appropriate functions.
+    
+    Args:
+        - on_key_down: The function to call when a key is pressed.
+        - on_mouse_down: The function to call when a mouse button is pressed.
+    
+    Returns:
+        - None
+    """
+    for event in pygame_event.get():
+        if event.type == QUIT:
+            quit_program()
+        elif event.type == KEYDOWN:
+            if on_key_down(event):
+                continue
+        elif event.type == MOUSEBUTTONDOWN:
+            if on_mouse_down(event):
+                continue
+
+def quit_program(status: int = 0) -> None:
+    """
+    Summary:
+    
+    Quits the program and exits with the given status.
+    
+    Args:
+        - status: The status to exit with.
+    
+    Returns:
+        - None
+    """
+    pygame_quit()
+    exit(status)
+
+def get_back_instruction_rect(text: str, color: tuple[int, int, int], position: tuple[int, int], back_font: font.Font, surface: Surface) -> Rect:
+    """
+    Summary:
+    
+    Gets the rectangle of the back instruction.
+    
+    Args:
+        - text: The text to render.
+        - color: The color of the back instruction.
+        - position: The center position (x, y) of the back instruction.
+        - back_font: The font to use for rendering the text.
+        - surface: The surface to draw the back instruction on.
+    
+    Returns:
+        - The rectangle of the back instruction surface.
+    """
+    back_surface = back_font.render(text, True, color)
+    back_rect = back_surface.get_rect(center=position)
+    surface.blit(back_surface, back_rect)
+    return back_rect
+
+def is_back_instruction_clicked(mouse_position: tuple[int, int], back_rect: Rect) -> bool:
+    """
+    Summary:
+    
+    Checks if the back instruction is clicked.
+    
+    Args:
+        - mouse_position: The position of the mouse.
+        - back_rect: The rectangle of the back instruction.
+    
+    Returns:
+        - True if the back instruction is clicked, False otherwise.
+    """ 
+    return back_rect.collidepoint(mouse_position)
